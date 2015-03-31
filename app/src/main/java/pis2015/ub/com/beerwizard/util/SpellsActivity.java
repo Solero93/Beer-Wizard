13d88bfc015f5c24277b8ccc227f88ca9f10aaae
Christian Soler <solero93@gmail.com>
2015-04-01 01:58:38 +0200

    Spells acabado. Yo Nico. Desde el ordenador de Christian

--- app/src/main/java/pis2015/ub/com/beerwizard/util/SpellsActivity.java
+++ app/src/main/java/pis2015/ub/com/beerwizard/util/SpellsActivity.java
@@ -1,11 +1,9 @@
 package pis2015.ub.com.beerwizard.util;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
 import pis2015.ub.com.beerwizard.R;
 
@@ -15,9 +13,8 @@ public class SpellsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spells);
-        Drawable d = getResources().getDrawable(R.drawable.cara);
-        ImageView logo = (ImageView) findViewById(android.R.id.home);
-        logo.setImageDrawable(d);
+        getSupportActionBar().setIcon(R.drawable.cara);//pone la cara en la barra
+        //setTitle("cacaman");//para cambiar el nombre de usuario
     }
 
 
@@ -25,7 +22,7 @@ public class SpellsActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_spells, menu);
-
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
         return super.onCreateOptionsMenu(menu);
 
     }
@@ -38,10 +35,15 @@ public class SpellsActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_profile) {
             return true;
         }
-
+        if (id == R.id.action_tutorial) {
+            return true;
+        }
+        if (id == R.id.action_exit) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
 }
