cdf6485d7d5bbd1a8e225b42f6aaa9d3215be853
Christian Soler <solero93@gmail.com>
2015-05-10 13:57:49 +0200

    [Christian] Fixed incorrect

--- app/src/main/java/pis2015/ub/com/beerwizard/game/spells/SpellShield.java
+++ app/src/main/java/pis2015/ub/com/beerwizard/game/spells/SpellShield.java
@@ -6,7 +6,7 @@ public class SpellShield extends Spell {
     public SpellShield() {
         setName(R.string.title_spell4);
         setImage(-1);
-        setDescription(R.string.description_quote_spell4);
+        setDescription(R.string.description_text_spell4);
         setQuote(R.string.description_quote_spell4);
         setLockedText(R.string.lock4);
         setLevelToUnlock(5);
