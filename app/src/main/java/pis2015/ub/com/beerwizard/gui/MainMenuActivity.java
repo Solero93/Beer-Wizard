c68db9c6efedc829b89fa8e8c61911cfd0a3b286
Christian Soler <solero93@gmail.com>
2015-05-11 10:09:18 +0200

    Reverting changes2

--- app/src/main/java/pis2015/ub/com/beerwizard/gui/MainMenuActivity.java
+++ app/src/main/java/pis2015/ub/com/beerwizard/gui/MainMenuActivity.java
@@ -97,7 +97,6 @@ public class MainMenuActivity extends Activity {
                 TextView clickedView = (TextView) view;
 */
                 Intent intent = new Intent(view.getContext(), SpellsActivity.class);
-                overridePendingTransition(R.anim.card_flip_left_in, R.anim.card_flip_left_out);
                 startActivity(intent);
             }
         });
