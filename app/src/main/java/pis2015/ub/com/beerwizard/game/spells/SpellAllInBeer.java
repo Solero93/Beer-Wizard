8ca758544f05c20e0197b7a9ad0e102942088624
Christian Soler <solero93@gmail.com>
2015-05-17 17:51:55 +0200

    [Christian] Renamed AllinShot -> AllinBeer

--- app/src/main/java/pis2015/ub/com/beerwizard/game/spells/SpellAllInBeer.java
+++ app/src/main/java/pis2015/ub/com/beerwizard/game/spells/SpellAllInBeer.java
@@ -2,8 +2,8 @@ package pis2015.ub.com.beerwizard.game.spells;
 
 import pis2015.ub.com.beerwizard.R;
 
-public class SpellAllInShot extends Spell {
-    public SpellAllInShot() {
+public class SpellAllInBeer extends Spell {
+    public SpellAllInBeer() {
         setName(R.string.title_spell8);
         setImage(-1);
         setDescription(R.string.description_text_spell8);
