9b3349a98b509a10a3b6f9997152ff28bef45dae
Administrador <al@al.com>
2015-05-17 12:19:33 +0200

    Profile Activity is nicer to seeeeeeeeeeee

--- app/src/main/java/pis2015/ub/com/beerwizard/gui/ProfileActivity.java
+++ app/src/main/java/pis2015/ub/com/beerwizard/gui/ProfileActivity.java
@@ -1,23 +1,22 @@
 package pis2015.ub.com.beerwizard.gui;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.PopupWindow;
 
 import pis2015.ub.com.beerwizard.R;
 
 
-public class ProfileActivity extends ActionBarActivity {
+public class ProfileActivity extends Activity {
 
     public static void enableDisableViewGroup(ViewGroup viewGroup, boolean enabled) {
         int childCount = viewGroup.getChildCount();
@@ -46,7 +45,9 @@ public class ProfileActivity extends ActionBarActivity {
             //                TextView wwa= (TextView)popupView.findViewById(R.id.)
             @Override
             public void onClick(View v) {
-
+                String name = ((EditText) findViewById(R.id.profileName)).getText().toString();
+                int idAvatar = 0;
+                GUIFacade.modifyUserProfile(name, idAvatar);
                 finish();
                 //overridePendingTransition(R.anim.card_flip_left_in, R.anim.card_flip_left_out);
             }
@@ -88,34 +89,12 @@ public class ProfileActivity extends ActionBarActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_profile, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void onClick_saveChanges(View view) {
-        //String name = ((EditText) findViewById(R.id.editText2)).getText().toString();
-        //int idAvatar = 0;
-        //GUIFacade.modifyUserProfile(name, idAvatar);
-        finish();
-    }
+//    public void onClick_saveChanges(View view) {
+//        String name = ((EditText) findViewById(R.id.profileName)).getText().toString();
+//
+//        finish();
+//    }
 
 }
 
