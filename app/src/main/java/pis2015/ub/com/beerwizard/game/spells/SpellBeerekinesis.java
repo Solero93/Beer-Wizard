70e90bc0d97e028549f9e5dd2cb7d27f5ef12635
Christian Soler <solero93@gmail.com>
2015-05-18 09:43:27 +0200

    [Christian] Added getImage method por Spells

--- app/src/main/java/pis2015/ub/com/beerwizard/game/spells/SpellBeerekinesis.java
+++ app/src/main/java/pis2015/ub/com/beerwizard/game/spells/SpellBeerekinesis.java
@@ -5,7 +5,7 @@ import pis2015.ub.com.beerwizard.R;
 public class SpellBeerekinesis extends Spell {
     public SpellBeerekinesis() {
         setName(R.string.title_spell3);
-        setImage(-1);
+        setImage(R.drawable.duel_of_wizards); // More images need to arrive
         setDescription(R.string.description_text_spell3);
         setQuote(R.string.description_quote_spell3);
         setLockedText(R.string.lock3);
--- app/src/main/java/pis2015/ub/com/beerwizard/game/spells/SpellCanToTheFace.java
+++ app/src/main/java/pis2015/ub/com/beerwizard/game/spells/SpellCanToTheFace.java
@@ -5,7 +5,7 @@ import pis2015.ub.com.beerwizard.R;
 public class SpellCanToTheFace extends Spell {
     public SpellCanToTheFace() {
         setName(R.string.title_spell1);
-        setImage(-1);
+        setImage(R.drawable.duel_of_wizards); // More images need to arrive
         setDescription(R.string.description_text_spell1);
         setQuote(R.string.description_quote_spell1);
         setLockedText(R.string.lock1);
