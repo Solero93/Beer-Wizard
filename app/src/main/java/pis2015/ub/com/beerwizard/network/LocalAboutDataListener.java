89c51be6a6394f9c0f6fc42bc6de73afe0a26389
Jordi Olivares <jordi@jolivar.es>
2015-05-29 23:17:34 +0200

    Refactor: Simplified the code a bit

--- app/src/main/java/pis2015/ub/com/beerwizard/network/LocalAboutDataListener.java
+++ app/src/main/java/pis2015/ub/com/beerwizard/network/LocalAboutDataListener.java
@@ -18,37 +18,37 @@ public class LocalAboutDataListener implements AboutDataListener {
     @Override
     public Map<String, Variant> getAboutData(String language)
             throws ErrorReplyBusException {
-        Map<String, Variant> aboutData = new HashMap<String, Variant>();
+        Map<String, Variant> aboutData = new HashMap<>();
         // nonlocalized values
         aboutData.put("AppId", new Variant(Constants.APP_ID));
-        aboutData.put("DefaultLanguage", new Variant(new String("en")));
+        aboutData.put("DefaultLanguage", new Variant("en"));
         aboutData.put("DeviceId", new Variant(Constants.UUID_STRING));
-        aboutData.put("ModelNumber", new Variant(new String("A1B2C3")));
+        aboutData.put("ModelNumber", new Variant("A1B2C3"));
         aboutData.put("SupportedLanguages", new Variant(new String[]{"en"}));
-        aboutData.put("DateOfManufacture", new Variant(new String("2014-09-23")));
+        aboutData.put("DateOfManufacture", new Variant("2014-09-23"));
         aboutData.put("SoftwareVersion", new Variant(Constants.VERSION_NUMBER));
         aboutData.put("AJSoftwareVersion", new Variant(Version.get()));
-        aboutData.put("HardwareVersion", new Variant(new String("0.1alpha")));
-        aboutData.put("SupportUrl", new Variant(new String("http://www.example.com/support")));
-        aboutData.put("DeviceName", new Variant(new String("A device name")));
-        aboutData.put("AppName", new Variant(new String("Beer Wizard")));
-        aboutData.put("Manufacturer", new Variant(new String("A mighty manufacturing company")));
+        aboutData.put("HardwareVersion", new Variant("0.1alpha"));
+        aboutData.put("SupportUrl", new Variant("http://www.example.com/support"));
+        aboutData.put("DeviceName", new Variant("A device name"));
+        aboutData.put("AppName", new Variant("Beer Wizard"));
+        aboutData.put("Manufacturer", new Variant("A mighty manufacturing company"));
         aboutData.put("Description",
-                new Variant(new String("Sample showing the about feature in a service application")));
+                new Variant("Sample showing the about feature in a service application"));
         return aboutData;
     }
 
     @Override
     public Map<String, Variant> getAnnouncedAboutData()
             throws ErrorReplyBusException {
-        Map<String, Variant> aboutData = new HashMap<String, Variant>();
+        Map<String, Variant> aboutData = new HashMap<>();
         aboutData.put("AppId", new Variant(Constants.APP_ID));
-        aboutData.put("DefaultLanguage", new Variant(new String("en")));
-        aboutData.put("DeviceName", new Variant(new String("A device name")));
+        aboutData.put("DefaultLanguage", new Variant("en"));
+        aboutData.put("DeviceName", new Variant("A device name"));
         aboutData.put("DeviceId", new Variant(Constants.UUID_STRING));
-        aboutData.put("AppName", new Variant(new String("Beer Wizard")));
-        aboutData.put("Manufacturer", new Variant(new String("A mighty manufacturing company")));
-        aboutData.put("ModelNumber", new Variant(new String("A1B2C3")));
+        aboutData.put("AppName", new Variant("Beer Wizard"));
+        aboutData.put("Manufacturer", new Variant("A mighty manufacturing company"));
+        aboutData.put("ModelNumber", new Variant("A1B2C3"));
         return aboutData;
     }
 }
